212840446b294ec8c9e70e82549d7c6a6a689c2e
Ghassan <g4ss4n@gmail.com>
2019-07-17 15:03:00 +0300

    css updates

--- dom.js
+++ dom.js
@@ -7,9 +7,7 @@
     var addTodoForm = document.getElementById('add-todo');
   
     var state = [
-      { id: -3, description: 'first todo' },
-      { id: -2, description: 'second todo' },
-      { id: -1, description: 'third todo' },
+
     ]; // this is our initial todoList
   
     // This function takes a todo, it returns the DOM node representing that todo
